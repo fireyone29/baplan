6173b5398a876f17369f1eed050edd40de149a97
Keegan Roth <keegangroth@gmail.com>
2017-02-28 20:10:42 -0500

    Add a quick hack to deal with timezone schenanigans.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module Baplan
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = ENV['TIMEZONE'] if ENV.has_key?('TIMEZONE')
   end
 end
